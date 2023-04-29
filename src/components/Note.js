0feb0b64b503496bfb393fc71a70377bbec30f7d
PlanetofJay <jae44dv@gmail.com>
2023-04-28 23:48:28 -0400

    using grid

--- src/components/Note.js
+++ src/components/Note.js
@@ -1,7 +1,14 @@
+import { RiDeleteBin6Line } from 'react-icons/ri';
 
 const Note = () => {
   return (
-    <div className="note"></div>
+    <div className="note">
+      <span>Hello! This is our first note!</span>
+      <div className="note-footer">
+        <small>14/02/2023</small>
+        <RiDeleteBin6Line className='delete-icon' size='1.3em' />
+      </div>
+    </div>
   );
 };
 export default Note;
