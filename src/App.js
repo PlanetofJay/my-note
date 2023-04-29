f33dc2c8f70ecf945c6eed5d6a407068ee324551
PlanetofJay <jae44dv@gmail.com>
2023-04-29 13:59:02 -0400

    fix hardcode

--- src/App.js
+++ src/App.js
@@ -1,12 +1,31 @@
-import NotesList from "./components/NotesList";
+import { useState } from 'react';
+import { nanoid } from 'nanoid';
+import NotesList from './components/NotesList';
 
 const App = () => {
+  const [notes, setNotes] = useState([
+    {
+      id: nanoid(),
+      text: 'This is my first note!',
+      date: '29/04/2023',
+    },
+    {
+      id: nanoid(),
+      text: 'This is my second note!',
+      date: '12/03/2023',
+    },
+    {
+      id: nanoid(),
+      text: 'This is my third note!',
+      date: '21/02/2023',
+    },
+  ]);
 
   return (
     <div className="container">
-      <NotesList />
+      <NotesList notes={notes} />
     </div>
-  )
+  );
 };
 
 export default App;
--- src/components/Note.js
+++ src/components/Note.js
@@ -1,11 +1,11 @@
 import { RiDeleteBin6Line } from 'react-icons/ri';
 
-const Note = () => {
+const Note = ({ id, text, date }) => {
   return (
-    <div className="note">
-      <span>Hello! This is our first note!</span>
-      <div className="note-footer">
-        <small>29/04/2023</small>
+    <div className='note'>
+      <span>{text}</span>
+      <div className='note-footer'>
+        <small>{date}</small>
         <RiDeleteBin6Line className='delete-icon' size='1.3em' />
       </div>
     </div>
